d75d4a9bacb3a684eb9e82973503bdbfdf5d7290
Baek-Hwan-Gi <abcy100@nate.com>
2018-04-15 23:15:05 +0900

    STAGE 3 Level Design
    Little

--- TeamProject/Assets/2DGamekit/Scripts/Character/MonoBehaviours/PlayerCharacter.cs
+++ TeamProject/Assets/2DGamekit/Scripts/Character/MonoBehaviours/PlayerCharacter.cs
@@ -37,6 +37,9 @@ namespace Gamekit2D
         public float jumpSpeed = 20f;
         public float jumpAbortSpeedReduction = 100f;
 
+       
+
+
         [Range(k_MinHurtJumpAngle, k_MaxHurtJumpAngle)] public float hurtJumpAngle = 45f;
         public float hurtJumpSpeed = 5f;
         public float flickeringDuration = 0.1f;
@@ -134,6 +137,8 @@ namespace Gamekit2D
             m_TanHurtJumpAngle = Mathf.Tan(Mathf.Deg2Rad * hurtJumpAngle);
             m_FlickeringWait = new WaitForSeconds(flickeringDuration);
 
+           
+
             meleeDamager.DisableDamage();
 
             m_ShotSpawnGap = 1f / shotsPerSecond;
@@ -156,6 +161,7 @@ namespace Gamekit2D
 
             m_StartingPosition = transform.position;
             m_StartingFacingLeft = GetFacing() < 0.0f;
+            
         }
 
         void OnTriggerEnter2D(Collider2D other)
@@ -177,6 +183,9 @@ namespace Gamekit2D
             }
         }
 
+       
+      
+
         void Update()
         {
             if (PlayerInput.Instance.Pause.Down)
@@ -197,7 +206,7 @@ namespace Gamekit2D
                     Unpause();
                 }
             }
-        }
+    }
 
         void FixedUpdate()
         { 
